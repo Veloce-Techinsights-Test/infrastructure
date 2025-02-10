0685de6e6dbeeae6332703b6ed14bf65d482e6e7
pranavpvelocetechinsights <pranav.p@velocetechinsights.com>
2025-02-10 16:30:47 +0530

    chore: remove dynamic-tables

--- projects/infrastructure/src/public-api.ts
+++ projects/infrastructure/src/public-api.ts
@@ -5,4 +5,3 @@
 export * from './lib/infrastructure.service';
 export * from './lib/infrastructure.component';
 export * from './lib/InfraDataClasss';
-export * from './lib/components/dynamic-table/dynamic-table.component'
