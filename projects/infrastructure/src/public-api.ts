9362c295eb1e03bda7443783a52e51cbd529b5ed
pranavpvelocetechinsights <pranav.p@velocetechinsights.com>
2025-02-10 16:42:09 +0530

    chore: add dynamic-table

--- projects/infrastructure/src/public-api.ts
+++ projects/infrastructure/src/public-api.ts
@@ -2,6 +2,11 @@
  * Public API Surface of infrastructure
  */
 
+// Export services, components, and classes
 export * from './lib/infrastructure.service';
 export * from './lib/infrastructure.component';
 export * from './lib/InfraDataClasss';
+export * from './lib/components/dynamic-table/dynamic-table.component';
+
+// Export the module to make components accessible
+export * from './lib/infrastructure.module';
